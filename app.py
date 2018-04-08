8cc41c3a6c7cc358bc8ca4896f74510966dd7a5a
Carlos Prados <carlos.prados@gmail.com>
2018-04-08 21:30:56 +0200

    First commit

---
+++ app.py
@@ -0,0 +1,24 @@
+import json
+from flask import Flask
+
+app = Flask(__name__)
+
+payload = {
+    'name': 'Status Quo'
+}
+index_payload = {
+    'name': 'I m CCarla'
+}
+
+
+@app.route('/rockband')
+def rock_band():
+    return json.dumps(payload)
+
+@app.route('/')
+def index():
+    return json.dumps(index_payload)
+
+
+if __name__ == '__main__':
+    pass
